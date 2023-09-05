f59e64a96db2779aab4addd14d9db623b42eaaa7
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-09-05 23:07:53 +0900

    Initial

---
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -0,0 +1,17 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.craftinginterpreters.lox.TokenType.*;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    Scanner(String source) {
+        this.source = source;
+    }
+}
