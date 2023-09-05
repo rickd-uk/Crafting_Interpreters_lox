3ed2a245b2226f458566802185bffed515bc1789
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-09-05 23:31:30 +0900

    Add: scanner - finished basic part

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -10,8 +10,25 @@ import com.craftinginterpreters.lox.TokenType.*;
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
 
     Scanner(String source) {
         this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            // We are at beginning of the next lexeme
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
     }
 }
