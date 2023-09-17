7d2b9d7be7a704dda71474cd998596825af150af
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-09-17 15:00:28 +0900

    Added 07 - Runtime Errors

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -7,16 +7,59 @@ public class Interpreter implements Expr.Visitor<Object> {
        return expr.value;
    }
 
-   @Override
+    @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitSetExpr(Expr.Set expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitSuperExpr(Expr.Super expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitThisExpr(Expr.This expr) {
+        return null;
+    }
+
+    @Override
    public Object visitUnaryExpr(Expr.Unary expr) {
        Object right = evaluate(expr.right);
 
-       return switch (expr.operator.type) {
-           case MINUS -> -(double) right;
-           case BANG -> !isTruthy(right);
-           default -> null;
+        switch (expr.operator.type) {
+           case MINUS:
+               checkNumberOperand(expr.operator, right);
+                   return -(double) right;
+           case BANG:
+               return !isTruthy(right);
        };
+       return null;
+   }
 
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitBinary(Expr.Binary expr) {
+        return null;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+       if (operand instanceof Double) return;
+       throw new RuntimeError(operator, "Operand must be a number.");
+   }
+
+   private void checkNumberOperands(Token operator, Object left, Object right) {
+       if (left instanceof Double && right instanceof Double) return;
+
+       throw new RuntimeError(operator, "Operands must be numbers.");
    }
 
    private Boolean isTruthy(Object object) {
@@ -41,39 +84,71 @@ public class Interpreter implements Expr.Visitor<Object> {
        return expr.accept(this);
    }
 
-   @Override
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        return null;
+    }
+
+    @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
        Object left = evaluate(expr.left);
        Object right = evaluate(expr.right);
 
-       switch(expr.operator.type) {
-           case GREATER:
-               return (double)left > (double)right;
-           case GREATER_EQUAL:
-               return (double)left >= (double)right;
-           case LESS:
-               return (double)left < (double)right;
-           case LESS_EQUAL:
-               return (double)left <= (double)right;
-           case MINUS:
-               return (double)left - (double)right;
-           case BANG_EQUAL:
-               return !isEqual(left, right);
-           case EQUAL_EQUAL:
-               return isEqual(left,right);
-           case PLUS:
-               if (left instanceof Double && right instanceof Double) {
-                   return (double)left + (double)right;
-           }
-               if (left instanceof String && right instanceof String) {
-                   return (String)left + (String)right;
-               }
-               break;
-           case SLASH:
-               return (double)left / (double)right;
-           case STAR:
-               return (double)left * (double)right;
-       }
+        switch (expr.operator.type) {
+            case GREATER -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left > (double) right;
+            }
+            case GREATER_EQUAL -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left >= (double) right;
+            }
+            case LESS -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left < (double) right;
+            }
+            case LESS_EQUAL -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left <= (double) right;
+            }
+            case MINUS -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left - (double) right;
+            }
+            case BANG_EQUAL -> {
+                return !isEqual(left, right);
+            }
+            case EQUAL_EQUAL -> {
+                return isEqual(left, right);
+            }
+            case PLUS -> {
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings");
+            }
+            case SLASH -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left / (double) right;
+            }
+            case STAR -> {
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left * (double) right;
+            }
+        }
        return null;
    }
+
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitGetExpr(Expr.Get expr) {
+        return null;
+    }
 }
